33b15ae8a2270f36d8a7ee629e43479bf7c4ec0d
Doholod <egorcherd16@gmail.com>
2025-01-21 22:09:35 +1000

    ADD: visual application

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QIntValidator"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lineEdit->setValidator(new QIntValidator(-1e9,1e9,this));
+    ui->lineEdit_2->setValidator(new QIntValidator(1,10,this));
 }
 
 MainWindow::~MainWindow()
